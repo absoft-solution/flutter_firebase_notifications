0c2b1807901cb4432def63a11d83545305d35ff1
Abdullah Anwar <info.ibnanwar@gmail.com>
2024-07-11 18:08:25 +0500

    Show notification in statusbar foreground background

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,31 +1,95 @@
+import 'dart:math';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class NotificationServices{
-  FirebaseMessaging firebaseMessaging=FirebaseMessaging.instance;
-  RequestNotification()async{
-NotificationSettings settings=  await firebaseMessaging.requestPermission(
-  alert: true,
-  badge: true,
-  announcement: true,
-  carPlay: true,
-  criticalAlert: true,
-  provisional: true,
-  sound: true,
+class NotificationServices {
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
-);
-if(settings.authorizationStatus==AuthorizationStatus.authorized){
-  print('user granted request');
-
-}
-else if(settings.authorizationStatus==AuthorizationStatus.provisional){
-  print('user granted provisional permission');
-}
-else{
-  print("user denied permission");
-}
+  Future<void> RequestNotification() async {
+    NotificationSettings settings = await firebaseMessaging.requestPermission(
+      alert: true,
+      badge: true,
+      announcement: true,
+      carPlay: true,
+      criticalAlert: true,
+      provisional: true,
+      sound: true,
+    );
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('user granted request');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('user granted provisional permission');
+    } else {
+      print("user denied permission");
+    }
   }
-  Future<String> GetDeviceToken() async{
-    String ? token= await firebaseMessaging.getToken();
-    return token!;
+
+  Future<String> GetDeviceToken() async {
+    String? token = await firebaseMessaging.getToken();
+    return token ?? 'No token received';
+  }
+
+  Future<void> initLocalNotification(
+      BuildContext context, RemoteMessage message) async {
+    var androidInitializationSettings =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iosInitializationSettings = const DarwinInitializationSettings();
+    var initializationSettings = InitializationSettings(
+      android: androidInitializationSettings,
+      iOS: iosInitializationSettings,
+    );
+
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: (NotificationResponse response) {
+      if (response.payload != null) {}
+    });
+  }
+
+  void firebaseInit() {
+    FirebaseMessaging.onMessage.listen((message) {
+      print(message.notification?.title ?? 'No title');
+      print(message.notification?.body ?? 'No body');
+      showNotification(message);
+    });
+  }
+
+  Future<void> showNotification(RemoteMessage message) async {
+    AndroidNotificationChannel channel = AndroidNotificationChannel(
+      Random.secure().nextInt(10000).toString(),
+      'Highly Important Notification',
+      description: 'This channel is used for important notifications.',
+      importance: Importance.high,
+    );
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      channel.id,
+      channel.name,
+      channelDescription: channel.description,
+      importance: Importance.high,
+      priority: Priority.high,
+      ticker: 'ticker',
+      icon: '@mipmap/ic_launcher',
+    );
+    DarwinNotificationDetails darwinNotificationDetails =
+        DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidNotificationDetails,
+      iOS: darwinNotificationDetails,
+    );
+
+    await _flutterLocalNotificationsPlugin.show(
+      0,
+      message.notification?.title ?? 'No title',
+      message.notification?.body ?? 'No body',
+      notificationDetails,
+    );
   }
 }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -14,6 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     notificationServices.RequestNotification();
+    notificationServices.firebaseInit();
 
 
   }
